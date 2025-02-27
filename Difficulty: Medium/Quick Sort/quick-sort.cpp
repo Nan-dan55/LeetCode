dd7f2e38b018177f45ec5253186e62967ea1556e
Nan-dan55 <146372355+Nan-dan55@users.noreply.github.com>
2025-02-27 16:38:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to print an array */
+void printArray(const vector<int>& arr) {
+    for (int num : arr)
+        printf("%d ", num);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to sort an array using quick sort algorithm.
+    void quickSort(vector<int>& arr, int low, int high) {
+        // code here
+        if(low<high){
+            int s=partition(arr,low,high);
+            quickSort(arr,low,s-1);
+            quickSort(arr,s+1,high);
+        }
+    }
+
+  public:
+    // Function that takes last element as pivot, places the pivot element at
+    // its correct position in sorted array, and places all smaller elements
+    // to left of pivot and all greater elements to right of pivot.
+    int partition(vector<int>& arr, int low, int high) {
+        // code here
+        int n=arr.size();
+        int i=low;
+        int j=high;
+        int val=arr[i];
+        while(i<=j){
+            while(i<=j && arr[i]<=val){
+                i++;
+            }
+             while(i<=j && arr[j]>val){
+                j--;
+            }
+            if(i<=j){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp=arr[j];
+        arr[j]=val;
+        arr[low]=temp;
+        return j;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    scanf("%d", &T);
+    getchar(); // to consume newline after T
+
+    while (T--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.quickSort(arr, 0, arr.size() - 1);
+        printArray(arr);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
