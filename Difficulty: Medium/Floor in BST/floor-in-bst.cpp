f6051d04d7ff2d5b803922f38eb4b9352e2c94d8
Nan-dan55 <146372355+Nan-dan55@users.noreply.github.com>
2025-06-21 12:57:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floor in BST/floor-in-bst.cpp
@@ -0,0 +1,21 @@
+// Function to search a node in BST.
+class Solution {
+
+  public:
+    int floor(Node* root, int x) {
+        // Code here
+        int floor=-1;
+        while(root){
+            if(root->data==x){
+                return x;
+            }else if(x<root->data){
+                
+                root=root->left;
+            }else{
+                floor=root->data;
+                root=root->right;
+            }
+        }
+        return floor;
+    }
+};
